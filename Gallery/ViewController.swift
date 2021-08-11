8db256a694c63744ca6b6ff087167f2104a65878
mohammed.ibrahim <mohammed.ibrahim@kydonlsg.com>
2021-08-11 08:18:31 +0530

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Mohammed Ibrahim on 11/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
